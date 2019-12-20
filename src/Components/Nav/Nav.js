0c9355819f8561414dbfae25061b9a7acbcd28fc
Paulzhong-web <paulzhong114@gmail.com>
2019-12-20 16:45:24 -0500

    nav-bar created

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-scroll';
+// import Logo from '../../Assets/Icon/paul-logo.svg';
 import './Nav.scss';
 
 export class Nav extends Component {
     render() {
         return (
             <nav className="navBar">
+                {/* <img className="navBar__logo" src={Logo} alt="logo"/> */}
                 <ul className="navBar__list">
                     <li className="navBar__list-item">
                         <Link
-                            activeClass="active"
+                            className="navBar__list-item-tab"
+                            activeClass="navBar__active"
                             to="about"
                             spy={true}
                             smooth={true}
@@ -18,7 +21,8 @@ export class Nav extends Component {
                     </li>
                     <li className="navBar__list-item">
                         <Link
-                            activeClass="active"
+                        className="navBar__list-item-tab"
+                            activeClass="navBar__active"
                             to="projects"
                             spy={true}
                             smooth={true}
@@ -27,7 +31,8 @@ export class Nav extends Component {
                     </li>
                     <li className="navBar__list-item">
                         <Link
-                            activeClass="active"
+                        className="navBar__list-item-tab"
+                            activeClass="navBar__active"
                             to="education"
                             spy={true}
                             smooth={true}
