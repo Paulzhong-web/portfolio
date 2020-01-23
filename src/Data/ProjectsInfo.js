f6828f7a6e64cbae68d87aa1ab977c6f5b1146ee
Paulzhong-web <paulzhong114@gmail.com>
2020-01-23 17:15:48 -0500

    fixed image size on Project Content

--- src/Data/ProjectsInfo.js
+++ src/Data/ProjectsInfo.js
@@ -7,13 +7,13 @@ export default [
         description: "This is a website designed and built for my band - WE ARE VARIOUS, which showcases the band's contends to its fans and audiences. It helps the band to show its unique style and identity, get people to sign up their email for updates and connect the fans together. (Mobile and Tablet version coming soon)",
         image: wavSite,
         websiteUrl: "http://www.wearevariousband.com/",
-        githubUrl: "http://www.wearevariousband.com/"
+        githubUrl: "https://github.com/Paulzhong-web/portfolio"
     },
-    {
-        name: "Coming Soon",
-        description: "Coming Soon",
-        image: testImg,
-        websiteUrl: "http://www.wearevariousband.com/",
-        githubUrl: "http://www.wearevariousband.com/"
-    },
+    // {
+    //     name: "Coming Soon",
+    //     description: "Coming Soon",
+    //     image: testImg,
+    //     websiteUrl: "http://www.wearevariousband.com/",
+    //     githubUrl: "http://www.wearevariousband.com/"
+    // },
 ]
